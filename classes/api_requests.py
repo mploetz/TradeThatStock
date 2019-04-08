e84cfbfa5269f4fabf74899256d39a1b44dcaac7
Matthew Ploetz <mploetz@indiana.edu>
2019-04-07 22:30:32 -0500

    Search endpoint completed of API class

--- classes/api_requests.py
+++ classes/api_requests.py
@@ -7,5 +7,16 @@ class API:
     def __init__(self):
         # Load in our api keys
         cwd = os.getcwd()
-        apiFile = open(cwd + '\\classes\\api_keys.json')
-        self.data = apiFile.read()
+        file = open(cwd + '\\classes\\api_keys.json')
+        apiFile = json.load(file)
+        self.data = apiFile
+        self.endpoints = apiFile["alphaVantage"]["endPoints"]
+
+    # Takes a string to search a stock by and returns an array of JSON best matches
+    def Search(self, name):
+        url = "https://www.alphavantage.co/query?"
+        payload = { 'function': self.endpoints["Search"], 'keywords': name, 'apikey': self.data["alphaVantage"]["apiKey"], 'datatype': 'json'}
+        r = requests.get(url, params=payload)
+        return r.json()
+
+print(API().Search("microsoft"))
